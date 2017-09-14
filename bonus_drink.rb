c631b1dd0299f0a72f240c4d30e0e99b7a107f3b
Kazuki Takeuchi <k.take57@gmail.com>
2017-09-14 22:16:14 +0900

    回答作成

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,13 @@
 class BonusDrink
+  @@BONUS_THLESHOLD = 3
+
   def self.total_count_for(amount)
-    0
+    drink_count = empty_count = amount
+    while empty_count >= @@BONUS_THLESHOLD
+      bonus_count, rest_count = empty_count.divmod(@@BONUS_THLESHOLD)
+      drink_count += bonus_count
+      empty_count = bonus_count + rest_count
+    end
+    drink_count
   end
 end
